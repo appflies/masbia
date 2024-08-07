cc353e5a6aa4ca8e76a3d9d30a9e192ab106fd26
Jonny Sixx <criptorockstar@gmail.com>
2024-08-07 08:31:39 -0300

    [building] process

--- resources/js/constants/images.js
+++ resources/js/constants/images.js
@@ -2,4 +2,7 @@ import logo from "../../assets/logo.svg";
 import slide1 from "../../assets/slide1.jpg";
 import slide2 from "../../assets/slide2.jpg";
 import slide3 from "../../assets/slide3.jpg";
-export { logo, slide1, slide2, slide3 };
+import quality from "../../assets/quality.svg";
+import about from "../../assets/about.png";
+import product from "../../assets/product.jpg";
+export { logo, slide1, slide2, slide3, quality, about, product };
